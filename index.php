893b331b814e2b493c076401e8d64e44913b8820
steven <steven_ik@hotmail.com>
2018-10-10 12:40:03 +0200

    made headers
    colors ar shit

--- index.php
+++ index.php
@@ -2,15 +2,6 @@
 include("includes/header.php");
  ?>
 
-<div class="alert alert-primary" role="alert">
-  Bootstrap test!
-</div>
-
-<?php
-    phpinfo();
-?>
-
-
 
 <?php
 include("includes/footer.php");
