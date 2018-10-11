a6fc4009928b62c3ec831f0c0b343cbfa581938b
Ivo <i.gerner@outlook.com>
2018-10-11 14:48:56 +0200

    Added forms and overviews from Ivo; Admin account order overview Admin specific order overview Admin edit specific order overview Customer payment selection

---
+++ pages/account_order.php
@@ -0,0 +1,35 @@
+<html>
+    Account Account# - Orders<br>
+    Last_name, First_name<br><br>
+
+  <table style="display: inline-block;">
+    <col width = 130><col width = 80>
+    <tr><td><b>Order Number</b></td><td align='right'>Order #</td></tr>
+    <tr><td><b>Date</b></td><td align='right'>##/##/####</td></tr>
+    <tr><td><b>Status</b></td><td align='right'>StatusID</td></tr>
+  </table>
+  <table style="display: inline-block;">
+    <col width = 80><col width = 150>
+    <tr><td><b>Address</b></td><td align='right'>First_name Last_name</td></tr>
+    <tr><td></td><td align='right'>Street #</td></tr>
+    <tr><td></td><td align='right'>City Postal_code</td></tr>
+    <tr><td></td><td align='right'>Country</td></tr>
+  </table>
+  <table style="display: inline-block;">
+    <col width = 130><col width = 130>
+    <tr><td><b>Payment Method</b></td><td align='right'>payment_method</td></tr>
+    <tr><td></td><td align='right'>email if PayPal</td></tr>
+  </table>
+  <br><br>
+  <table>
+    <col width = 180><col width = 50>
+    <tr><td><b>Products</b><td></tr>
+    <tr><td>Product #1</td><td align='right'>#1 $$</td></tr>
+    <tr><td>Product #2</td><td align='right'>#2 $$</td></tr>
+    <tr><td>Product #3</td><td align='right'>#3 $$</td></tr>
+    <tr><td><b>Total</b></td><td align='right'>$$$$</td></tr>
+  </table>
+  <form method="post" action="account_order_edit.php">
+    <input type="submit" name="edit" value="Edit order">
+  </form>
+</html>
---
+++ pages/payment_selection.php
@@ -0,0 +1,56 @@
+<html>
+  <h3>Order overview</h3>
+  <table>
+    <?php
+    //   foreach($key as $value){
+    //     echo "<tr><td>$value</td><td>$$</td></tr>";
+    // }
+    ?>
+    <tr><td>Product 1</td><td>$$</td></tr>
+    <tr><td>Product 2</td><td>$$</td></tr>
+    <tr><td><b>Total:</b></td><td><b>$$$$</b></td></tr>
+  </table>
+  <br>
+  <p><h3>Delivery address</h3></p>
+  <table>
+    First_name Last_name<br>
+    Street #<br>
+    City Postal_code<br>
+    Country<br>
+  <br>
+  <p><h3>Select payment method</h3></p>
+  <form method = 'post'>
+    <table>
+      <tr><td><input type="radio" name="payment_method" value="paypal">Paypal</td>
+      <td><input type="radio" name="payment_method" value="ideal">iDeal</td>
+      <td><input type="radio" name="payment_method" value="creditcard">Credit Card</td>
+      <td><input type="radio" name="payment_method" value="bitcoin">Bitcoin</td></tr>
+      </table>
+      <br />
+      <input type="submit" name="submit" value="Confirm">
+  </form>
+
+
+
+</html>
+<?php
+if(isset($_POST['submit'])){
+  if(!isset($_POST['payment_method'])){
+    echo "Please select a payment method.";
+  }
+  else{
+    if($_POST['payment_method']=='paypal'){
+      echo "PayPal";
+    }
+    elseif($_POST['payment_method']=='ideal'){
+      echo "iDeal";
+    }
+    elseif($_POST['payment_method']=='creditcard'){
+      echo "Credit Card";
+    }
+    elseif($_POST['payment_method']=='bitcoin'){
+      echo "Bitcoin";
+    }
+  }
+}
+?>
