397aa432666138862f7e1555c33ce282e510e5ca
steven <steven_ik@hotmail.com>
2018-10-17 12:52:00 +0200

    made function from header

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,14 @@
 <?php
-include("ddb_connect.php");
+function get_header($title,$discription ){
 
- ?>
+echo <<<EOT
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
-    <title>Title</title>
+    <title>{$title}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="{$discription}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="/kaasch/css/main.css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
@@ -39,3 +40,7 @@ include("ddb_connect.php");
           </div>
         </div>
   </nav>
+EOT;
+}
+
+  ?>
--- index.php
+++ index.php
@@ -1,34 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <?php
-      $path = $_SERVER["DOCUMENT_ROOT"];
-      require($path . "/includes/head.php");
-    ?>
-    <title>Kaasch Home</title>
-</head>
-<body>
-    <?php require($path . "/includes/nav.php"); ?>
-    <header class="masthead text-center text-white d-flex">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-10 mx-auto">
-            <h1 class="text-uppercase">
-              <strong>Kaasch Webshop</strong>
-            </h1>
-          </div>
+
+<?php
+  require("/includes/header.php");
+  get_header('kaasch', 'kaasisbaas');
+?>
+
+  <header class="masthead text-center text-white d-flex">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-10 mx-auto">
+          <h1 class="text-uppercase">
+            <strong>Kaasch Webshop</strong>
+          </h1>
         </div>
       </div>
-    </header>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1 style="height: 1200px;">Homepage</h1>
-            </div>
-        </div>
-    <div>
-    <?php require($path . "/includes/footer.php"); ?>
-</body>
-</html>
-
-
+    </div>
+  </header>
+  <div class="container">
+      <div class="row">
+          <div class="col-12">
+              <h1 style="height: 1200px;">Homepage</h1>
+          </div>
+      </div>
+  </div>
+  <?php require("/includes/footer.php"); ?>
