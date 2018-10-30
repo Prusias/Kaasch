f714c4f4446d15cdcebd55f32e225cd1d805e02b
Ivo <i.gerner@outlook.com>
2018-10-30 10:56:42 +0100

    Account order overview

--- pages/account_order_overview.php
+++ pages/account_order_overview.php
@@ -1,41 +1,58 @@
 <?php
 	require_once("../document_root.php");
+	require ('../includes/databases.php');
 
-	require_once(get_document_root() . "/includes/header.php");
+	require_once(get_document_root() . "/kaasch/includes/header.php");
 	get_header('kaasch', '');
+	$sql = (
+		"SELECT `users_id`, o.`id`, `date`, `status_id`, `first_name`, `last_name`, sum(`price`*`amount`) AS `total`
+		FROM `orders` o
+			JOIN `users` u ON o.`id` = u.`id`
+    	JOIN `orders_has_products` ohp ON o.`id` = ohp.`orders_id`
+    	JOIN `products` p ON ohp.`products_id` = p.`id`;
+			");
+			$result = mysqli_query($db, $sql);
+			if (mysqli_num_rows($result) > 0){
+				while($row = mysqli_fetch_assoc($result)){
+					echo ("
+					<table>
+					<tr>
+						<td><b>ID </b>".$row['users_id']."</td>
+						</tr>
+					<tr>
+						<td>".$row['first_name']."</td><td>".$row['last_name']."</td>
+					</tr>
+					</table>
+					<h3>Order overview</h3>
+					  <table>
+					    <tr>
+					      <td><b>Order Number</b></td>
+					      <td><b>Date</b></td>
+					      <td><b>Order Total</b></td>
+					      <td><b>Status</b></td>
+					      <td></td>
+					    </tr>
+					");
+				}
+				$result = mysqli_query($db, $sql);
+				while($row = mysqli_fetch_assoc($result)){
+					if ($row['status_id'] == 1){
+						$status = 'Processing';
+					}
+					echo ("
+					<tr>
+						<td>".$row['id']."</td>
+						<td>".$row['date']."</td>
+						<td>".$row['total']."</td>
+						<td>".$status."</td>
+						<td><b>></b></td>
+					</tr>
+					");
+				}
+				echo "</table>";
+			}
+			else {
+				echo "geen records gevonden";
+			}
 ?>
-
-    Account Account# - Orders<br>
-    Last_name, First_name
-
-<h3>Order overview</h3>
-  <table>
-    <tr>
-      <td><b>Order Number</b></td>
-      <td><b>Date</b></td>
-      <td><b>Order Total</b></td>
-      <td><b>Status</b></td>
-      <td></td>
-    </tr>
-    <tr><td>Order #1</td>
-      <td>Date Order #1</td>
-      <td>Sum Products Order #1</td>
-      <td>Status Order #1</td>
-      <td><b>></b></td>
-    </tr>
-    <tr>
-      <td>Order #2</td>
-      <td>Date Order #2</td>
-      <td>Sum Products Order #2</td
-        ><td>Status Order #2</td>
-        <td><b>></b></td>
-      </tr>
-    <tr>
-      <td>Order #3</td>
-      <td>Date Order #3</td>
-      <td>Sum Products Order #3</td>
-      <td>Status Order #3</td>
-      <td><b>></b></td>
-    </tr>
-  </table>
-  <?php require_once(get_document_root() . "/includes/footer.php"); ?>
+  <?php require_once(get_document_root() . "/kaasch/includes/footer.php"); ?>
