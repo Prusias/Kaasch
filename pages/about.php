897bc8b11ec7f98ea613832a1d232726361d13c5
Ivo <i.gerner@outlook.com>
2018-11-13 13:29:25 +0100

    Fixed language use

--- pages/about.php
+++ pages/about.php
@@ -9,14 +9,14 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-           <h2>Kaasch about</h2>
-			The reason for creating the webshop Kaasch, occurs from our love for cheese. We saw a hole in the cheese market, 
-			namely that not everyone can enjoy all the delicious Dutch cheese outside the Netherlands. 
-			For this dilemma is the webshop in Kaasch, Dutch cheese to anyone outside the Netherlands to enjoy the best and the finest Dutch cheeses. 
-			The current situation is that there aren't good webshops, where consumers outside the Netherlands can order Dutch cheeses. 
-			Our target group are mainly young households; men and women from 18 years of age who life outside the Netherlands, 
-			with a great love for all kinds of various cheese. Most cheese eaters who order primarily online, are the young House holders. 
-			The problem that the webshop solves is the difficult import of Dutch cheese, by an accessible website.<br>
+           <h2>About Kaasch</h2>
+			The reason behind creating the webshop Kaasch is because of our love for cheese. We saw a gap in the cheese market
+			namely that not everyone can enjoy all the delicious Dutch cheese outside of the Netherlands.
+			To find a solution created the webshop Kaasch, Dutch cheese to anyone outside of the Netherlands. This way everyone can enjoy the best and the finest Dutch cheeses.
+			The current situation is that there aren't any good webshops where consumers outside of the Netherlands can order Dutch cheeses.
+			Our target group are mainly young households; men and women from 18 years of age who live outside of the Netherlands,
+			with a great interest for all kinds of various cheese. Most cheese eaters who order primarily online are the young Households.
+			The problem that the webshop solves is the difficult import of Dutch cheese using an accessible website.<br>
 			<img src=../images/cheese.jpg height=600 width=1100>
 
         </div>
